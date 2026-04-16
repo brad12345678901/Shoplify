7e84706f845922a4008cf2b66f87280e94f7ed46
Brad <ibanezbradley@gmail.com>
2026-04-17 02:23:04 +0800

    Added Categories Controller
    - Created Read Operation API for Categories

---
+++ shoplify-backend/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using shoplify_backend.Data;
+using shoplify_backend.Models;
+
+namespace shoplify_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController(ShoplifyContext db) : ControllerBase
+    {
+        private readonly ShoplifyContext _db = db;
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _db
+                .Category.Where(category => category.Deleted_At == null)
+                .Select(category => new { id = category.Id, category_nam = category.Name })
+                .ToListAsync();
+
+            var response = new
+            {
+                success = true,
+                message = "Categories fetched Successfully",
+                data = categories,
+            };
+
+            return Ok(response);
+        }
+    }
+}
